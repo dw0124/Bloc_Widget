8439320d9c303a833aace9fa00616f7dd173371d
dw0124 <doowone8650@naver.com>
2025-04-21 23:40:22 +0900

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:bloc_widget/counter/counter.dart';
+import 'package:bloc_widget/timer/view/timer_app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const CounterApp());
+  //runApp(const CounterApp());
+  runApp(const TimerApp());
 }
