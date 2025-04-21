7524959638de0a946dd1f7014c706c329777c79d
dw0124 <doowone8650@naver.com>
2025-04-21 23:40:11 +0900

    Timer 화면 구현

---
+++ lib/timer/view/timer_app.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:bloc_widget/timer/view/timer_page.dart';
+
+class TimerApp extends MaterialApp {
+  const TimerApp({super.key}) : super(home: const TimerPage());
+}
