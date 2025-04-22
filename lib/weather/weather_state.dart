a5f91d38399a88e2e10417477af8b2fe8430f022
dw0124 <doowone8650@naver.com>
2025-04-22 21:16:53 +0900

    WeatherState - copyWith 메소드 추가

--- lib/weather/weather_state.dart
+++ lib/weather/weather_state.dart
@@ -5,11 +5,18 @@ enum WeatherStatus { initial, loading, success, failure }
 class WeatherState extends Equatable {
 
   //Weather weather;
-  WeatherStatus weatherStatus;
+  final WeatherStatus weatherStatus;
 
-  WeatherState({
+  const WeatherState({
     this.weatherStatus = WeatherStatus.initial
   });
+
+  // 기존 인스턴스의 값을 유지하면서, 특정 프로퍼티만 변경하여 새로운 객체를 생성
+  WeatherState copyWith({WeatherStatus? weatherStatus}) {
+    return WeatherState(
+      weatherStatus: weatherStatus ?? this.weatherStatus,
+    );
+  }
 
   @override
   List<Object?> get props => [weatherStatus];
