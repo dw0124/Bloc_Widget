cfd08137ebcb703554000e7cba52c52c28b53791
dw0124 <doowone8650@naver.com>
2025-04-24 23:32:16 +0900

    Repository - Provider를 관리하고, 제공된 rawData를 가공하여 bloc, cubit에 제공함

---
+++ lib/weather/repository/weather_repository.dart
@@ -0,0 +1,17 @@
+import 'package:bloc_widget/weather/models/weather.dart';
+import 'package:bloc_widget/weather/provider/weather_api_provider.dart';
+
+class WeatherRepository {
+  WeatherRepository({WeatherApiProvider? weatherApiProvider})
+      : _weatherApiProvider = weatherApiProvider ?? WeatherApiProvider();
+
+  final WeatherApiProvider _weatherApiProvider;
+
+  Future<Weather> fetchWeather() async {
+    final Map<String, dynamic> weatherJson = await _weatherApiProvider.fetchWeatherJson();
+
+    Weather weather = Weather.fromJson(weatherJson);
+
+    return weather;
+  }
+}
