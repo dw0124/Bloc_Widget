4612d4a388345dadf4f0f9ed1d18746de20a60cd
dw0124 <doowone8650@naver.com>
2025-04-28 00:17:55 +0900

    WeatherCondition extension으로 Weather.weatherCondition 매핑

--- lib/weather/models/weather.dart
+++ lib/weather/models/weather.dart
@@ -1,4 +1,35 @@
-enum WeatherCondition {clear, rainy, cloudy, snowy, unknown,}
+enum WeatherCondition {
+  clear,
+  clouds,
+  rain,
+  snow,
+  thunderstorm,
+  drizzle,
+  atmosphere,
+  unknown,
+}
+
+extension WeatherConditionX on WeatherCondition {
+  static WeatherCondition fromWeatherCode(int code) {
+    if (code >= 200 && code < 300) {
+      return WeatherCondition.thunderstorm;
+    } else if (code >= 300 && code < 400) {
+      return WeatherCondition.drizzle;
+    } else if (code >= 500 && code < 600) {
+      return WeatherCondition.rain;
+    } else if (code >= 600 && code < 700) {
+      return WeatherCondition.snow;
+    } else if (code >= 700 && code < 800) {
+      return WeatherCondition.atmosphere;
+    } else if (code == 800) {
+      return WeatherCondition.clear;
+    } else if (code >= 801 && code < 900) {
+      return WeatherCondition.clouds;
+    } else {
+      return WeatherCondition.unknown;
+    }
+  }
+}
 
 enum TemperatureUnits { fahrenheit, celsius }
 
@@ -12,31 +43,34 @@ class Weather {
   final double temperature;
   final double minTemperature;
   final double maxTemperature;
-  final String description;
+  final WeatherCondition weatherCondition;
 
   const Weather({
     required this.cityName,
     required this.temperature,
     required this.minTemperature,
     required this.maxTemperature,
-    required this.description,
+    required this.weatherCondition,
   });
 
   factory Weather.fromJson(Map<String, dynamic> json) {
+    final int weatherCode = json['weather'][0]['id'] as int;
+    WeatherCondition weatherCondition = WeatherConditionX.fromWeatherCode(weatherCode);
+
     return Weather(
       cityName: json['name'] as String,
       temperature: (json['main']['temp'] as num).toDouble(),
       minTemperature: (json['main']['temp_min'] as num).toDouble(),
       maxTemperature: (json['main']['temp_max'] as num).toDouble(),
-      description: (json['weather'][0]['description']) as String,
+      weatherCondition: weatherCondition,
     );
   }
 
   static final empty = Weather(
-    cityName: '',
-    temperature: 0,
-    minTemperature: 0,
-    maxTemperature: 0,
-    description: "unknown"
+      cityName: '',
+      temperature: 0,
+      minTemperature: 0,
+      maxTemperature: 0,
+      weatherCondition: WeatherCondition.unknown
   );
 }
