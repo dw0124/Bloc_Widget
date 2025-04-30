2a4c4a84ea3f7d56e160951a4d9a34160d17d5f5
dw0124 <doowone8650@naver.com>
2025-05-01 00:54:17 +0900

    한국어 날짜, 포맷 처리와 관련된 초기화

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:bloc_widget/counter/counter.dart';
 import 'package:bloc_widget/timer/view/timer_app.dart';
+import 'package:bloc_widget/weather/view/weather_app.dart';
 import 'package:flutter/material.dart';
 
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 void main() async {
+  await initializeDateFormatting('ko');
   await dotenv.load(fileName: ".env");
-
   //runApp(const CounterApp());
-  runApp(const TimerApp());
+  //runApp(const TimerApp());
+  runApp(const WeatherApp());
 }
