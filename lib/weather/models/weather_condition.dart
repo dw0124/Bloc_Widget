286952b9b31a61d4876f972bf769642590509433
dw0124 <doowone8650@naver.com>
2025-05-02 23:50:24 +0900

    WeatherCondtion을 통해 이미지 경로, 텍스트 리턴하는 getter 추가

--- lib/weather/models/weather_condition.dart
+++ lib/weather/models/weather_condition.dart
@@ -29,4 +29,48 @@ extension WeatherConditionX on WeatherCondition {
       return WeatherCondition.unknown;
     }
   }
+
+  /// 날씨 이미지 경로
+  String get imageAsset {
+    switch (this) {
+      case WeatherCondition.clear:
+        return 'assets/weather_icon/clear.png';
+      case WeatherCondition.clouds:
+        return 'assets/weather_icon/clouds.png';
+      case WeatherCondition.rain:
+        return 'assets/weather_icon/rain.png';
+      case WeatherCondition.snow:
+        return 'assets/weather_icon/snow.png';
+      case WeatherCondition.thunderstorm:
+        return 'assets/weather_icon/thunderstorm.png';
+      case WeatherCondition.drizzle:
+        return 'assets/weather_icon/rain.png';
+      case WeatherCondition.atmosphere:
+        return 'assets/weather_icon/atmosphere.png';
+      default:
+        return 'assets/weather_icon/unknown.png';
+    }
+  }
+
+  /// 날씨 텍스트
+  String get label {
+    switch (this) {
+      case WeatherCondition.clear:
+        return '맑음';
+      case WeatherCondition.clouds:
+        return '흐림';
+      case WeatherCondition.rain:
+        return '비';
+      case WeatherCondition.snow:
+        return '눈';
+      case WeatherCondition.thunderstorm:
+        return '천둥';
+      case WeatherCondition.drizzle:
+        return '이슬비';
+      case WeatherCondition.atmosphere:
+        return '안개';
+      default:
+        return '알 수 없음';
+    }
+  }
 }
