e687ea59668334701113d6a9e256325f1e3eb05f
dw0124 <doowone8650@naver.com>
2025-05-06 23:54:52 +0900

    DailyWeatherListItem

---
+++ lib/weather/view/widgets/daily_weather_list_item.dart
@@ -0,0 +1,48 @@
+import 'package:bloc_widget/weather/models/daily_weather.dart';
+import 'package:bloc_widget/weather/models/weather_condition.dart';
+import 'package:flutter/material.dart';
+
+class DailyWeatherListItem extends StatelessWidget {
+  final DailyWeather dailyWeather;
+
+  const DailyWeatherListItem({super.key, required this.dailyWeather});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return SizedBox(
+      height: 64,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        spacing: 24,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                  dailyWeather.weekdayString,
+                  style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Text('data'),
+            ],
+          ),
+          SizedBox(
+              width: 64,
+              height: 64,
+              child: Image.asset(dailyWeather.weatherCondition.imageAsset)
+          ),
+          Text(
+              dailyWeather.weatherCondition.label,
+              style: TextStyle(color: Colors.black, fontSize: 16, fontWeight: FontWeight.bold)
+          ),
+          Expanded(child: SizedBox()),
+          Text(
+              '${dailyWeather.maxTemperature} / ${dailyWeather.minTemperature}',
+              style: TextStyle(color: Colors.black, fontSize: 16, fontWeight: FontWeight.bold)
+            ),
+        ],
+      ),
+    );
+  }
+}
