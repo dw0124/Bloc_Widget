f0586bb4bd9ec7b379e99263b0101d679382198d
dw0124 <doowone8650@naver.com>
2025-05-07 23:08:30 +0900

    기본 형태 화면 구현 SingleChildScrollView + Column을 사용하여 현재, 시간별, 날짜별 데이터 표시

--- lib/weather/view/weather_page.dart
+++ lib/weather/view/weather_page.dart
@@ -1,8 +1,11 @@
 import 'package:bloc_widget/weather/bloc/weather_cubit.dart';
 import 'package:bloc_widget/weather/bloc/weather_state.dart';
 import 'package:bloc_widget/weather/models/current_weather.dart';
+import 'package:bloc_widget/weather/models/daily_weather.dart';
+import 'package:bloc_widget/weather/models/hourly_weather.dart';
 import 'package:bloc_widget/weather/models/weather_condition.dart';
-import 'package:bloc_widget/weather/repository/weather_repository.dart';
+import 'package:bloc_widget/weather/view/widgets/daily_weather_list_item.dart';
+import 'package:bloc_widget/weather/view/widgets/hourly_weather_list_item.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -13,24 +16,110 @@ class WeatherPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Center(
-          child: BlocSelector<WeatherCubit, WeatherState, CurrentWeather>(
-              selector: (state) => state.weather.current,
-              builder: (context, currentWeather) {
-                return Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Image.asset(currentWeather.weatherCondition.imageAsset),
-                        Text(currentWeather.temperature.toString()),
-                      ],
-                    ),
-                    Text(currentWeather.weatherCondition.label),
-                  ],
-                );
-              }
-          )
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(height: 30,),
+
+                BlocSelector<WeatherCubit, WeatherState, CurrentWeather>(
+                    selector: (state) => state.weather.current,
+                    builder: (context, currentWeather) {
+                      return Column(
+                        children: [
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Text(
+                                '${currentWeather.temperature.toString()}°',
+                                style: TextStyle(fontSize: 64.0, fontWeight: FontWeight.w300),
+                              ),
+                              Transform.translate(
+                                offset: Offset(0, -20),
+                                child: Text(
+                                  currentWeather.weatherCondition.label,
+                                  style: TextStyle(fontSize: 24.0),
+                                )
+                              ),
+                              Transform.translate(
+                                offset: Offset(0, -20),
+                                child: Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    Text('최고: ${context.read<WeatherCubit>().state.weather.daily[0].minTemperature}°'),
+                                    Text('최저: ${context.read<WeatherCubit>().state.weather.daily[0].maxTemperature}°'),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      );
+                    }
+                ),
+
+                SizedBox(height: 30,),
+
+                Container(
+                  alignment: Alignment.center,
+                  height: 114,
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: BlocSelector<WeatherCubit, WeatherState, List<HourlyWeather>>(
+                      selector: (state) => state.weather.hourly,
+                      builder: (context, hourlyWeather) {
+                        return ListView.separated(
+                          padding: EdgeInsets.fromLTRB(8, 8, 0, 8),
+                          itemCount: hourlyWeather.length,
+                          scrollDirection: Axis.horizontal,
+                          itemBuilder: (context, index) {
+                            return HourlyWeatherListItem(
+                              key: ValueKey(hourlyWeather[index].epochTime),
+                              hourlyWeather: hourlyWeather[index],
+                            );
+                          }, separatorBuilder: (BuildContext context, int index) => SizedBox(width: 16)
+                        );
+                      }
+                  ),
+                ),
+
+                SizedBox(height: 12,),
+
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: BlocSelector<WeatherCubit, WeatherState, List<DailyWeather>>(
+                      selector: (state) => state.weather.daily,
+                      builder: (context, dailyWeather) {
+                        return ListView.separated(
+                          padding: EdgeInsets.fromLTRB(8, 0, 8, 0),
+                            itemCount: dailyWeather.length,
+                            scrollDirection: Axis.vertical,
+                            shrinkWrap: true,
+                            primary: false,
+                            itemBuilder: (context, index) {
+                              return DailyWeatherListItem(
+                                key: ValueKey(dailyWeather[index].epochTime),
+                                // 고유한 키 사용
+                                dailyWeather: dailyWeather[index],
+                              );
+                            },
+                            separatorBuilder: (BuildContext context, int index) => Divider(
+                              color: Colors.white.withValues(alpha: 0.3),
+                              height: 1,
+                            ),);
+                      }
+                  ),
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
