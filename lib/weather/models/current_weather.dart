5225199ebc3c8a0ebe7988399fc97f158d8f1adf
dw0124 <doowone8650@naver.com>
2025-05-07 23:58:28 +0900

    sunset, sunrise, epochTime(현재시간) 추가

--- lib/weather/models/current_weather.dart
+++ lib/weather/models/current_weather.dart
@@ -1,12 +1,20 @@
 import 'weather_condition.dart';
 
 class CurrentWeather {
+  final DateTime epochTime;
+  final DateTime sunrise;
+  final DateTime sunset;
   final int temperature;
   final WeatherCondition weatherCondition;
 
   factory CurrentWeather.fromJson(Map<String, dynamic> json) {
 
     try {
+      final DateTime epochTime = DateTime.fromMillisecondsSinceEpoch(json['dt'] * 1000, isUtc: true).toLocal();
+
+      final DateTime sunrise = DateTime.fromMillisecondsSinceEpoch(json['sunrise'] * 1000, isUtc: true).toLocal();
+      final DateTime sunset = DateTime.fromMillisecondsSinceEpoch(json['sunset'] * 1000, isUtc: true).toLocal();
+
       final double temperature = json['temp'];
 
       // 시간별 날씨 상태 - 맑음, 비, 구름 ...
@@ -15,6 +23,9 @@ class CurrentWeather {
           weatherCode);
 
       return CurrentWeather(
+        epochTime: epochTime,
+        sunrise: sunrise,
+        sunset: sunset,
         temperature: temperature.round(),
         weatherCondition: weatherCondition,
       );
@@ -29,12 +40,18 @@ class CurrentWeather {
   }
 
   CurrentWeather({
+    required this.epochTime,
+    required this.sunrise,
+    required this.sunset,
     required this.temperature,
     required this.weatherCondition
   });
 
   static final empty = CurrentWeather(
-      temperature: 0,
-      weatherCondition: WeatherCondition.unknown
+    epochTime: DateTime.fromMillisecondsSinceEpoch(1746598815 * 1000, isUtc: true).toLocal(),
+    sunrise: DateTime.fromMillisecondsSinceEpoch(1746563555 * 1000, isUtc: true).toLocal(),
+    sunset: DateTime.fromMillisecondsSinceEpoch(1746613471 * 1000, isUtc: true).toLocal(),
+    temperature: 0,
+    weatherCondition: WeatherCondition.unknown
   );
 }
