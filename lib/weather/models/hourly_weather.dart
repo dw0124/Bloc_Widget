83080ffc3d0483680f1304c128b556dabb780d1d
dw0124 <doowone8650@naver.com>
2025-05-07 23:58:48 +0900

    sunset, sunrise 추가

--- lib/weather/models/hourly_weather.dart
+++ lib/weather/models/hourly_weather.dart
@@ -3,21 +3,20 @@ import 'package:intl/intl.dart';
 import 'weather_condition.dart';
 
 class HourlyWeather {
-  final int epochTime;
+  final DateTime epochTime;
+  final DateTime sunrise;
+  final DateTime sunset;
   final String dateTimeString;
   final int temperature;
   final WeatherCondition weatherCondition;
   final double pop;
 
-  factory HourlyWeather.fromJson(Map<String, dynamic> json) {
+  factory HourlyWeather.fromJson({required Map<String, dynamic> json, required sunrise, required sunset}) {
     try {
-      // 시간 정보 - 1745982000
-      final int epochTime = json['dt'];
-
       // 시간 표시 - 오후 1시, 오후 3시 ...
-      final DateTime dateTime = DateTime.fromMillisecondsSinceEpoch(
-          epochTime * 1000, isUtc: true).toLocal();
-      final String formattedTime = DateFormat('a h시', 'ko').format(dateTime);
+      final DateTime epochTime = DateTime.fromMillisecondsSinceEpoch(
+          json['dt'] * 1000, isUtc: true).toLocal();
+      final String formattedTime = DateFormat('a h시', 'ko').format(epochTime);
 
       // 시간별 온도
       final int temperature = json['temp'].round();
@@ -30,6 +29,8 @@ class HourlyWeather {
       final double pop = (json['pop'] as num).toDouble();
 
       return HourlyWeather(
+        sunrise: sunrise,
+        sunset: sunset,
         epochTime: epochTime,
         dateTimeString: formattedTime,
         temperature: temperature,
@@ -47,6 +48,8 @@ class HourlyWeather {
 
   HourlyWeather({
     required this.epochTime,
+    required this.sunrise,
+    required this.sunset,
     required this.dateTimeString,
     required this.temperature,
     required this.weatherCondition,
@@ -54,7 +57,9 @@ class HourlyWeather {
   });
 
   static final empty = HourlyWeather(
-    epochTime: 1745982000,
+    epochTime: DateTime.fromMillisecondsSinceEpoch(1745982000 * 1000, isUtc: true).toLocal(),
+    sunrise: DateTime.fromMillisecondsSinceEpoch(1746563555 * 1000, isUtc: true).toLocal(),
+    sunset: DateTime.fromMillisecondsSinceEpoch(1746613471 * 1000, isUtc: true).toLocal(),
     dateTimeString: '오후 ?시',
     temperature: 0,
     weatherCondition: WeatherCondition.unknown,
