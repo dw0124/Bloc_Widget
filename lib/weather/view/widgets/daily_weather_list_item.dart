4c2d341773de9e5b0dfedd718cbd97f04c75dbbb
dw0124 <doowone8650@naver.com>
2025-05-09 21:31:40 +0900

    글씨색 변경, rain일때 강수확률 text 추가

--- lib/weather/view/widgets/daily_weather_list_item.dart
+++ lib/weather/view/widgets/daily_weather_list_item.dart
@@ -12,36 +12,63 @@ class DailyWeatherListItem extends StatelessWidget {
     // TODO: implement build
     return SizedBox(
       height: 64,
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        spacing: 24,
-        children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                  dailyWeather.weekdayString,
-                  style: TextStyle(fontWeight: FontWeight.bold),
+      child: DefaultTextStyle(
+        style: TextStyle(color: Colors.white),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          spacing: 24,
+          children: [
+            SizedBox(
+              width: 30,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    dailyWeather.weekdayString,
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    dailyWeather.formattedTime,
+                    style: TextStyle(color: Colors.white.withValues(alpha: 0.7)),
+                  ),
+                ],
               ),
-              Text(dailyWeather.formattedTime),
-            ],
-          ),
-          SizedBox(
-              width: 64,
-              height: 64,
-              child: Image.asset(dailyWeather.weatherCondition.imageAsset())
-          ),
-          Text(
-              dailyWeather.weatherCondition.label,
-              style: TextStyle(color: Colors.black, fontSize: 16, fontWeight: FontWeight.bold)
-          ),
-          Expanded(child: SizedBox()),
-          Text(
-              '${dailyWeather.minTemperature} / ${dailyWeather.maxTemperature}',
-              style: TextStyle(color: Colors.black, fontSize: 16, fontWeight: FontWeight.bold)
             ),
-        ],
+            Container(
+                height: 64,
+                //padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        dailyWeather.weatherCondition.imageAsset(),
+                        fit: BoxFit.contain,
+                        width: 32,
+                        height: 32,
+                      ),
+
+                      dailyWeather.weatherCondition == WeatherCondition.rain ?
+                      Text(
+                          '${(dailyWeather.pop * 100).toInt()}%',
+                          style: TextStyle(fontSize: 12),
+
+                      )
+                          : SizedBox(width: 0,)
+                    ]
+                )
+            ),
+            Text(
+                dailyWeather.weatherCondition.label,
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)
+            ),
+            Expanded(child: SizedBox()),
+            Text(
+                '${dailyWeather.minTemperature} / ${dailyWeather.maxTemperature}',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)
+              ),
+          ],
+        ),
       ),
     );
   }
