d7d4c7a36b38e84b4721bad39bd9cd577fde3cf4
dw0124 <doowone8650@naver.com>
2025-05-15 22:53:40 +0900

    .

--- lib/weather/repository/weather_repository.dart
+++ lib/weather/repository/weather_repository.dart
@@ -1,19 +1,28 @@
+import 'package:bloc_widget/weather/models/location_address.dart';
 import 'package:bloc_widget/weather/models/weather.dart';
+import 'package:bloc_widget/weather/provider/reverse_geocoding_api_provider.dart';
 import 'package:bloc_widget/weather/provider/weather_api_provider.dart';
 
 class WeatherRepository {
-  WeatherRepository({WeatherApiProvider? weatherApiProvider})
-      : _weatherApiProvider = weatherApiProvider ?? WeatherApiProvider();
+  WeatherRepository({WeatherApiProvider? weatherApiProvider, ReverseGeocodingApiProvider? reverseGeocodingApiProvider})
+  : _weatherApiProvider = weatherApiProvider ?? WeatherApiProvider(),
+    _reverseGeocodingApiProvider = reverseGeocodingApiProvider ?? ReverseGeocodingApiProvider();
 
-  final WeatherApiProvider _weatherApiProvider;
+final ReverseGeocodingApiProvider _reverseGeocodingApiProvider;
+final WeatherApiProvider _weatherApiProvider;
 
-  Future<Weather> fetchWeather() async {
-    final Map<String, dynamic> weatherJson = await _weatherApiProvider.fetchWeatherJson();
+Future<(Weather, LocationAddress)> fetchLocationInfo(double lng, double lat) async {
+  final Map<String, dynamic> weatherJson = await _weatherApiProvider.fetchWeatherJson(lng: lng, lat: lat);
+  Weather weather = Weather.fromJson(weatherJson);
 
-    Weather weather = Weather.fromJson(weatherJson);
+  final Map<String, dynamic> locationAddressJson = await _reverseGeocodingApiProvider.fetchWeatherJson(lng, lat);
+  LocationAddress locationAddress = LocationAddress.fromJson(locationAddressJson, lat, lng);
 
-    return weather;
-  }
+  return (weather, locationAddress);
+}
 
-  void dispose() => _weatherApiProvider.close();
+void dispose() {
+  _weatherApiProvider.close();
+  _reverseGeocodingApiProvider.close();
+}
 }
