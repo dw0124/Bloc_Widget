531b7b88e0a7e458e3d70cd2ca655f76f953491d
dw0124 <doowone8650@naver.com>
2025-05-15 22:53:04 +0900

    좌표 기준으로 데이터를 불러오도록 수정

--- lib/weather/bloc/weather_cubit.dart
+++ lib/weather/bloc/weather_cubit.dart
@@ -1,24 +1,29 @@
 import 'package:bloc/bloc.dart';
 import 'package:bloc_widget/weather/bloc/weather_state.dart';
+import 'package:bloc_widget/weather/models/location_address.dart';
+import 'package:bloc_widget/weather/models/weather.dart';
+import 'package:bloc_widget/weather/repository/map_repository.dart';
 import 'package:bloc_widget/weather/repository/weather_repository.dart';
 
 class WeatherCubit extends Cubit<WeatherState> {
-  WeatherCubit(this._weatherRepository) : super(WeatherState()) {
-    fetchWeather();
+  WeatherCubit(this._weatherRepository, double lng, double lat) : super(WeatherState()) {
+    fetchWeather(lng, lat);
   }
 
   final WeatherRepository _weatherRepository;
 
-  Future<void> fetchWeather() async {
+  Future<void> fetchWeather(double lng, double lat) async {
     // 로딩 중 상태로 변경
     emit(state.copyWith(weatherStatus: WeatherStatus.loading));
 
     try {
-      final weather = await _weatherRepository.fetchWeather();
+      final (weather, locationAddress) = await _weatherRepository.fetchLocationInfo(lng, lat);
       emit(
         state.copyWith(
             weatherStatus: WeatherStatus.success,
-            weather: weather)
+            weather: weather,
+            locationAddress: locationAddress
+        )
       );
     } on Exception {
       emit(
