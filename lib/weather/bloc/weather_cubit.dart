40dc27c68ec11292df9cbc05494fa409479d9da8
dw0124 <doowone8650@naver.com>
2025-05-21 23:24:00 +0900

    초기화 내부 함수로 뺌

--- lib/weather/bloc/weather_cubit.dart
+++ lib/weather/bloc/weather_cubit.dart
@@ -6,11 +6,26 @@ import 'package:bloc_widget/weather/repository/map_repository.dart';
 import 'package:bloc_widget/weather/repository/weather_repository.dart';
 
 class WeatherCubit extends Cubit<WeatherState> {
-  WeatherCubit(this._weatherRepository, double lng, double lat) : super(WeatherState()) {
-    fetchWeather(lng, lat);
+  WeatherCubit(this._weatherRepository) : super(WeatherState()) {
+    _init();
   }
 
   final WeatherRepository _weatherRepository;
+
+  Future<void> _init() async {
+    final loaded = await _loadSavedWeather();
+
+    if (loaded) {
+      final lng = state.locationAddress.longitude;
+      final lat = state.locationAddress.latitude;
+      fetchWeather(lng, lat); // 갱신
+    } else {
+      // 로컬에 아무것도 없을 때 -> 기본값
+      const defaultLat = 37.5665;
+      const defaultLng = 126.9780;
+      fetchWeather(defaultLng, defaultLat);
+    }
+  }
 
   Future<void> fetchWeather(double lng, double lat) async {
     // 로딩 중 상태로 변경
