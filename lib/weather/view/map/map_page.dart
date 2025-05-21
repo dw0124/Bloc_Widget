8e11fb8cf09b56f797daa3f9662c7e78430a7ee7
dw0124 <doowone8650@naver.com>
2025-05-21 23:05:24 +0900

    State 변경시 마커, 시간별 예보 리빌드

--- lib/weather/view/map/map_page.dart
+++ lib/weather/view/map/map_page.dart
@@ -1,3 +1,5 @@
+import 'package:bloc_widget/weather/bloc/map_cubit.dart';
+import 'package:bloc_widget/weather/bloc/map_state.dart';
 import 'package:bloc_widget/weather/bloc/weather_cubit.dart';
 import 'package:bloc_widget/weather/bloc/weather_state.dart';
 import 'package:bloc_widget/weather/models/hourly_weather.dart';
@@ -48,14 +50,16 @@ class _MapPageState extends State<MapPage> {
           },
         ),
         actions: [
-          TextButton(
-              child: Text('적용'),
-              onPressed: () {
-                Navigator.pop(
-                    context,
-                    (_weather, _locationAddress)
-                );
-              },
+          IconButton(
+            icon: const Icon(
+              Icons.check,
+            ),
+            onPressed: () {
+              Navigator.pop(
+                  context,
+                  (_weather, _locationAddress)
+              );
+            },
           )
         ],
       ),
@@ -98,8 +102,11 @@ class _MapPageState extends State<MapPage> {
                     final lat = latLng.latitude;
                     final lng = latLng.longitude;
 
-                    final (weather, locationAddress) = await context.read<MapRepository>()
-                        .fetchLocationInfo(lng, lat);
+                    await context.read<MapCubit>().fetchWeather(lng, lat);
+
+                    final state = context.read<MapCubit>().state;
+                    final weather = state.weather;
+                    final locationAddress = state.locationAddress;
 
                     final locationWeatherMarker = await NOverlayImage.fromWidget(
                         widget: LocationWeatherMarker(
@@ -107,7 +114,8 @@ class _MapPageState extends State<MapPage> {
                             locationAddress: locationAddress
                         ),
                         size: const Size(130, 140),
-                        context: context);
+                        context: context
+                    );
 
                     final marker = NMarker(
                         id: "icon_test",
@@ -117,9 +125,7 @@ class _MapPageState extends State<MapPage> {
 
                     _mapController.addOverlay(marker);
 
-                    _weather = weather;
-                    _locationAddress = locationAddress;
-
+                    // 카메라 업데이트
                     final cameraUpdate = NCameraUpdate.scrollAndZoomTo(
                       target: NLatLng(lat, lng),
                     );
@@ -131,21 +137,51 @@ class _MapPageState extends State<MapPage> {
                 Positioned(
                   left: 12,
                   right: 12,
-                  bottom: 0,
+                  bottom: 12,
                   child: Container(
-                    color: Colors.blue,
-                      height: 120,
-                      child: ListView.separated(
-                          padding: EdgeInsets.fromLTRB(8, 8, 0, 8),
-                          itemCount: _weather.hourly.length,
-                          scrollDirection: Axis.horizontal,
-                          itemBuilder: (context, index) {
-                            return HourlyWeatherListItem(
-                              key: ValueKey(_weather.hourly[index].epochTime),
-                              hourlyWeather: _weather.hourly[index],
-                            );
-                          }, separatorBuilder: (BuildContext context, int index) => SizedBox(width: 16)
-                      )
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Column(
+                      children: [
+                        Container(
+                          alignment: Alignment.centerLeft,
+                          padding: EdgeInsets.fromLTRB(8, 8, 0, 0),
+                          height: 32,
+                            child: BlocBuilder<MapCubit, MapState>(
+                              builder: (context, state) {
+                                return Text(
+                                  '${state.locationAddress.area1} ${state.locationAddress
+                                      .area2} ${state.locationAddress.area3}',
+                                  style: TextStyle(fontSize: 16,
+                                      fontWeight: FontWeight.bold),
+                                );
+                              }
+                            )
+                        ),
+                        Divider(),
+                        Container(
+                            height: 112,
+                            child: BlocBuilder<MapCubit, MapState>(
+                              builder: (context, state) {
+                                return ListView.separated(
+                                    padding: EdgeInsets.fromLTRB(8, 0, 0, 0),
+                                    itemCount: state.weather.hourly.length,
+                                    scrollDirection: Axis.horizontal,
+                                    itemBuilder: (context, index) {
+                                      return HourlyWeatherListItem(
+                                        key: ValueKey(state.weather.hourly[index].epochTime),
+                                        hourlyWeather: state.weather.hourly[index],
+                                        textColor: Colors.black,
+                                      );
+                                    }, separatorBuilder: (BuildContext context, int index) => SizedBox(width: 16)
+                                );
+                              }
+                            )
+                        ),
+                      ],
+                    ),
                   ),
                 )
             ]
