f8c61fb62cf6be1c807d60bfb17f42d9876089a2
dw0124 <doowone8650@naver.com>
2025-05-23 14:54:19 +0900

    iOS - WeatherWidget 추가

---
+++ ios/WeatherWidget/WeatherWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherWidgetBundle.swift
+//  WeatherWidget
+//
+//  Created by 김두원 on 5/23/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WeatherWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        WeatherWidget()
+    }
+}
