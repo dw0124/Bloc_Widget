b43bf59aae9d591a3ed32c5622d14b7f3bb9f78e
dw0124 <doowone8650@naver.com>
2025-05-26 23:54:42 +0900

    iOS - WeatherWidgetSmall 화면 구현

--- ios/WeatherWidget/Views/Widgets/WeatherWidgetSmall.swift
+++ ios/WeatherWidget/Views/Widgets/WeatherWidgetSmall.swift
@@ -9,11 +9,35 @@ import SwiftUI
 
 struct WeatherWidgetSmall: View {
     
+    let weather: Weather
+    
     var body: some View {
-        Text("WeatherWidgetSmall")
+        VStack(alignment: .leading) {
+            VStack(alignment: .leading) {
+                Text("서울특별시")
+                    .font(.system(size: 20))
+                
+                Text("\(weather.current.temperature)º")
+                    .font(.system(size: 40))
+            }
+            
+            Spacer()
+            
+            
+            VStack(alignment: .leading) {
+                Image(weather.current.weatherCondition.imageAsset())
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 24, height: 24)
+                Text(weather.current.weatherCondition.label)
+                    .font(Font.system(size: 14, weight: .semibold))
+                Text("최고:\(weather.daily.first!.maxTemperature)º 최저:\(weather.daily.first!.minTemperature)º")
+                    .font(Font.system(size: 14, weight: .semibold))
+            }
+        }
     }
 }
 
 #Preview {
-    WeatherWidgetSmall()
+    WeatherWidgetSmall(weather: Weather.dummy)
 }
