3968060359d5d8d466321004a2d830dfdc1a503f
dw0124 <doowone8650@naver.com>
2025-05-26 16:58:53 +0900

    iOS - large사이즈 위젯 화면 구현

--- ios/WeatherWidget/Views/Widgets/WeatherWidgetLarge.swift
+++ ios/WeatherWidget/Views/Widgets/WeatherWidgetLarge.swift
@@ -9,11 +9,69 @@ import SwiftUI
 
 struct WeatherWidgetLarge: View {
     
+    let weather: Weather
+    
+    var weeklyHighTemp: Int {
+        return weather.daily.map { $0.maxTemperature }.max() ?? 0
+    }
+    
+    var weeklyLowTemp: Int {
+        return weather.daily.map { $0.minTemperature }.min() ?? 0
+    }
+    
     var body: some View {
-        Text("WeatherWidgetLarge")
+        VStack(spacing: 8) {
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("서울특별시")
+                        .font(.system(size: 20))
+                    
+                    Text("\(weather.current.temperature)º")
+                        .font(.system(size: 40))
+                }
+                
+                Spacer()
+                
+                
+                VStack(alignment: .trailing) {
+                    Image(weather.current.weatherCondition.imageAsset())
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 24, height: 24)
+                    Text(weather.current.weatherCondition.label)
+                        .font(Font.system(size: 14, weight: .semibold))
+                    Text("최고:\(weather.daily.first!.maxTemperature)º 최저:\(weather.daily.first!.minTemperature)º")
+                        .font(Font.system(size: 14, weight: .semibold))
+                }
+            }
+            
+            VStack {
+                Divider()
+                
+                HStack {
+                    ForEach(weather.hourly.prefix(6), id: \.self.epochTime) { weather in
+                        HourlyWeatherItem(weather: weather)
+                            .frame(maxWidth: .infinity)
+                    }
+                }
+                
+                Divider()
+            }
+            
+            ForEach(weather.daily.prefix(5), id: \.self.epochTime) { weather in
+                DailyWeatherItem(
+                    weather: weather,
+                    weeklyHighTemp: weeklyHighTemp,
+                    weeklyLowTemp: weeklyLowTemp
+                )
+                .frame(maxWidth: .infinity)
+            }
+            
+        }
+        .frame(maxWidth: .infinity)
     }
 }
 
 #Preview {
-    WeatherWidgetLarge()
+    WeatherWidgetLarge(weather: Weather.dummy)
 }
