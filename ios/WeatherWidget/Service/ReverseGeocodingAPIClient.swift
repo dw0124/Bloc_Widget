235003d3157d57b137022a081db3ca23a72a7cfb
dw0124 <doowone8650@naver.com>
2025-05-27 02:07:17 +0900

    .

---
+++ ios/WeatherWidget/Service/ReverseGeocodingAPIClient.swift
@@ -0,0 +1,8 @@
+//
+//  ReverseGeocodingAPIClient.swift
+//  Runner
+//
+//  Created by 김두원 on 5/27/25.
+//
+
+import Foundation
---
+++ ios/WeatherWidget/Service/WeatherAPIClient.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherAPIClient.swift
+//  Runner
+//
+//  Created by 김두원 on 5/27/25.
+//
+
+import Foundation
