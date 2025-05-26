70318321d3ef1f9f967dc3f5b6a1543d363a5034
dw0124 <doowone8650@naver.com>
2025-05-26 23:54:32 +0900

    iOS - WeatherWidgetMedium 화면 구현

--- ios/WeatherWidget/Views/Widgets/WeatherWidgetMedium.swift
+++ ios/WeatherWidget/Views/Widgets/WeatherWidgetMedium.swift
@@ -9,11 +9,44 @@ import SwiftUI
 
 struct WeatherWidgetMedium: View {
     
+    let weather: Weather
+    
     var body: some View {
-        Text("WeatherWidgetMedium")
+        VStack(spacing: 8) {
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("서울특별시")
+                        .font(.system(size: 20))
+                    
+                    Text("\(weather.current.temperature)º")
+                        .font(.system(size: 40))
+                }
+                
+                Spacer()
+                
+                
+                VStack(alignment: .trailing) {
+                    Image(weather.current.weatherCondition.imageAsset())
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 24, height: 24)
+                    Text(weather.current.weatherCondition.label)
+                        .font(Font.system(size: 14, weight: .semibold))
+                    Text("최고:\(weather.daily.first!.maxTemperature)º 최저:\(weather.daily.first!.minTemperature)º")
+                        .font(Font.system(size: 14, weight: .semibold))
+                }
+            }
+            
+            HStack {
+                ForEach(weather.hourly.prefix(6), id: \.self.epochTime) { weather in
+                    HourlyWeatherItem(weather: weather)
+                        .frame(maxWidth: .infinity)
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    WeatherWidgetMedium()
+    WeatherWidgetMedium(weather: Weather.dummy)
 }
