15f158a69da233647f35395fc10cdc70ddac0aae
dw0124 <doowone8650@naver.com>
2025-05-27 17:36:25 +0900

    iOS - WeatherService: ReverseGeocodingAPIClient, WeatherAPIClient를 관리하는 클래스

--- ios/WeatherWidget/Service/WeatherService.swift
+++ ios/WeatherWidget/Service/WeatherService.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+class WeatherService {
+    let reverseGeocodingAPIClient: ReverseGeocodingAPIClient
+    let weatherAPIClient: WeatherAPIClient
+    
+    init(reverseGeocodingAPIClient: ReverseGeocodingAPIClient?, weatherAPIClient: WeatherAPIClient?) {
+        self.reverseGeocodingAPIClient = reverseGeocodingAPIClient ?? ReverseGeocodingAPIClient()
+        self.weatherAPIClient = weatherAPIClient ?? WeatherAPIClient()
+    }
+    
+    func fetchWeather(lat: Double, lng: Double) async throws -> (Weather, LocationAddress) {
+        var weather: Weather
+        var locationAddress: LocationAddress
+        
+        let locationAddressData = try await reverseGeocodingAPIClient.fetchLocationAddressData(lat: lat, lng: lng)
+        let locationAddressEntity = try JSONDecoder().decode(LocationAddressEntity.self, from: locationAddressData)
+        locationAddress = LocationAddress.fromEntity(entity: locationAddressEntity, lat: lat, lng: lng)
+        
+        let weatherData = try await weatherAPIClient.fetchWeatherData(lat: lat, lng: lng)
+        let weatherEntity = try JSONDecoder().decode(WeatherEntity.self, from: weatherData)
+        weather = Weather.fromEntity(entity: weatherEntity)
+        
+        return (weather, locationAddress)
+    }
+}
